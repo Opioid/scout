87103a5a0eada9fc7be56ee376b1d631f4d4f049
beni <benjamin.savs@gmail.com>
2014-06-23 19:35:30 +0200

    Experimental image output

--- scout/scout.go
+++ scout/scout.go
@@ -4,6 +4,10 @@ import (
 	"github.com/Opioid/scout/base/math"
 	"github.com/Opioid/scout/base/math/bounding"
 	"fmt"
+	"os"
+	"image"
+	"image/color"
+	"image/png"
 )
 
 func main() {
@@ -20,4 +24,34 @@ func main() {
 	fmt.Printf("The result is %v, that's a vector\n", c)
 
 	fmt.Printf("dot(a, b) == %f\n", math.V3Dot(a, b))
+
+	fo, err := os.Create("output.png")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer fo.Close()
+
+//	out := bufio.NewWriter(fo)
+
+//	defer out.Flush()
+
+//	out.WriteString("Umpa lumpa")
+
+	width := 320
+	height := 240
+
+	image := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			r := uint8(255.0 * (float32(y) / float32(height)))
+			g := uint8(255.0 * (float32(x) / float32(width)))
+
+			image.Set(x, y, color.RGBA{r, g, 127, 255})
+		}
+	}
+
+	png.Encode(fo, image)
 }
