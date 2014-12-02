a083f6bd21ba6bbe3e1139c0caea4ec18cd4bb4d
beni <benjamin.savs@gmail.com>
2014-12-02 11:58:14 +0100

    Improved performance of SUI saving/loading

--- core/rendering/texture/load.go
+++ core/rendering/texture/load.go
@@ -1,7 +1,10 @@
 package texture
 
 import (
+	"github.com/Opioid/scout/base/math"
+	gomath "math"
 	"io"
+	"bufio"
 	"bytes"
 	"encoding/binary"
 	"errors"
@@ -9,9 +12,11 @@ import (
 )
 
 func Load(r io.Reader) (*Texture2D, error) {
+	br := bufio.NewReader(r)
+
 	header := make([]byte, 3)
 
-	if _, err := r.Read(header); err != nil {
+	if _, err := io.ReadFull(br, header); err != nil {
 		return nil, err
 	}
 
@@ -20,30 +25,32 @@ func Load(r io.Reader) (*Texture2D, error) {
 	}
 
 	description := description{}
-	if err := binary.Read(r, binary.LittleEndian, &description); err != nil {
+	if err := binary.Read(br, binary.LittleEndian, &description); err != nil {
 		return nil, err
 	}
 
 	texture := NewTexture2DFromDescription(&description)
 
+	buf := make([]byte, 4 * 4)
+
 	buffers := texture.Image.Buffers
-	for i := range texture.Image.Buffers {
-		if err := binary.Read(r, binary.LittleEndian, buffers[i].Data()); err != nil {
-			return nil, err
-		}
-	}
-
-/*
-	if err := binary.Write(w, binary.LittleEndian, t.description()); err != nil {
-		return err
-	}
-
-	buffers := t.Image.Buffers
 	for i := range buffers {
-		if err := binary.Write(w, binary.LittleEndian, buffers[i].Data()); err != nil {
-			return err
+		dimensions := buffers[i].Dimensions()
+		for y := int32(0); y < dimensions.Y; y++ {
+			for x := int32(0); x < dimensions.X; x++ {
+				if _, err := io.ReadFull(br, buf); err != nil {
+					return nil, err
+				}
+
+				r := gomath.Float32frombits(binary.LittleEndian.Uint32(buf[ 0: 4]))
+				g := gomath.Float32frombits(binary.LittleEndian.Uint32(buf[ 4: 8]))
+				b := gomath.Float32frombits(binary.LittleEndian.Uint32(buf[ 8:12]))
+				a := gomath.Float32frombits(binary.LittleEndian.Uint32(buf[12:16]))
+
+				buffers[i].Set(x, y, math.MakeVector4(r, g, b, a))
+			}
 		}
 	}
-*/
+
 	return texture, nil
 }
--- core/rendering/texture/save.go
+++ core/rendering/texture/save.go
@@ -1,23 +1,44 @@
 package texture
 
 import (
+	_ "github.com/Opioid/scout/base/math"
+	gomath "math"
 	"io"
+	"bufio"
 	"encoding/binary"
 )
 
 func Save(w io.Writer, t *Texture2D) error {
-	w.Write([]byte{'S', 'U', 'I'})
+	bw := bufio.NewWriter(w)
 
-	if err := binary.Write(w, binary.LittleEndian, t.description()); err != nil {
+	bw.Write([]byte{'S', 'U', 'I'})
+
+	if err := binary.Write(bw, binary.LittleEndian, t.description()); err != nil {
 		return err
 	}
 
+	buf := make([]byte, 4 * 4)
+
 	buffers := t.Image.Buffers
 	for i := range buffers {
-		if err := binary.Write(w, binary.LittleEndian, buffers[i].Data()); err != nil {
-			return err
+		dimensions := buffers[i].Dimensions()
+		for y := int32(0); y < dimensions.Y; y++ {
+			for x := int32(0); x < dimensions.X; x++ {
+				c := buffers[i].At(x, y)
+
+				binary.LittleEndian.PutUint32(buf[ 0: 4], gomath.Float32bits(c.X))
+				binary.LittleEndian.PutUint32(buf[ 4: 8], gomath.Float32bits(c.Y))
+				binary.LittleEndian.PutUint32(buf[ 8:12], gomath.Float32bits(c.Z))
+				binary.LittleEndian.PutUint32(buf[12:16], gomath.Float32bits(c.W))
+
+				if _, err := bw.Write(buf); err != nil {
+					return err
+				}
+			}
 		}
 	}
+
+	bw.Flush()
 
 	return nil
 }
