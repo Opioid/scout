10c0460fdc78ccc36767b3d1142152161ae85ccf
beni <benjamin.savs@gmail.com>
2015-01-15 11:46:53 +0100

    Added a comment.

--- core/scene/prop/prop.go
+++ core/scene/prop/prop.go
@@ -8,7 +8,11 @@ import (
 
 type Prop struct {
 	Shape shape.Shape
-	AABB bounding.AABB
+
+	// This is intended to hold the world space AABB.
+	// Therefore it actually only makes sense for static props because it is dependent on time.
+	// Maybe move there.
+	AABB bounding.AABB	
 
 	Materials []material.Material
 }
