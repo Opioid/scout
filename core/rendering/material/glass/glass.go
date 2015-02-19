d457fe284ef625d68b1e4f7630fdea85f29f97fd
beni <benjamin.savs@gmail.com>
2015-02-19 16:37:27 +0100

    Experiments

--- core/rendering/material/glass/glass.go
+++ core/rendering/material/glass/glass.go
@@ -3,7 +3,7 @@ package glass
 import (
 	"github.com/Opioid/scout/core/rendering/sampler"
 	"github.com/Opioid/scout/core/rendering/material"
-	_ "github.com/Opioid/scout/core/rendering/material/ggx"
+	"github.com/Opioid/scout/core/rendering/material/ggx"
 	"github.com/Opioid/scout/base/math"
 	"github.com/Opioid/math32"
 	_ "math"
@@ -31,12 +31,13 @@ type Sample struct {
 	values material.Values
 
 	btdf Btdf
+	brdf Brdf
 }
 
 func NewSample() *Sample {
 	s := &Sample{}
 	s.btdf.sample = s
-//	s.ggx.sample = s
+	s.brdf.sample = s
 	return s
 }
 
@@ -53,8 +54,21 @@ func (s *Sample) Values() *material.Values {
 }
 
 func (s *Sample) SampleEvaluate(subsample uint32, sampler sampler.Sampler) (math.Vector3, math.Vector3, float32) {
-	r, wi, _, pdf := s.btdf.ImportanceSample(subsample, sampler)
-	return r, wi, pdf	
+	p := sampler.GenerateSample1D(0, 0)
+
+	if p < 0.5 {
+		r, wi, _, pdf := s.btdf.ImportanceSample(subsample, sampler)
+		return r, wi, pdf * 0.5
+	} else {
+		r, wi, _, pdf := s.brdf.ImportanceSample(subsample, sampler)
+		return r, wi, pdf * 0.5
+	}
+
+//	r, wi, _, pdf := s.btdf.ImportanceSample(subsample, sampler)
+//	return r, wi, pdf
+
+//	r, wi, _, pdf := s.brdf.ImportanceSample(subsample, sampler)
+//	return r, wi, pdf	
 }
 
 type Btdf struct {
@@ -97,21 +111,17 @@ func (b *Btdf) ImportanceSample(subsample uint32, sampler sampler.Sampler) (math
 
 	wi := t.Normalized()
 
-	return b.sample.values.DiffuseColor, wi, cosi, 1.0
+//	return b.sample.values.DiffuseColor, wi, cosi, 1.0
 
-/*
-	f0 := math.MakeVector3(0.3, 0.3, 0.3)
 
 	h := b.sample.values.Wo.Add(wi).Normalized()
-
 	WoDotH := b.sample.values.Wo.Dot(h)
 
+	f0 := math.MakeVector3(0.03, 0.03, 0.03)
 	fresnel := ggx.SpecularF(WoDotH, f0)
 
-	return b.sample.values.DiffuseColor.Scale(1.0 - fresnel.X).Div(wi.Dot(n)), wi, 1.0
-*/
-	
-	
+	return b.sample.values.DiffuseColor.Mul(fresnel), wi, 1.0, 1.0
+
 }
 
 func (b *Btdf) Evaluate(l math.Vector3, NdotWi float32) (math.Vector3, float32) {
@@ -138,3 +148,24 @@ inline void Refract(
   else
     out = eta * incidentVec - (eta * N_dot_I + sqrtf(k)) * N;
 }*/
+
+
+type Brdf struct {
+	sample *Sample
+}
+
+func (b *Brdf) Evaluate(l math.Vector3, NdotWi float32) (math.Vector3, float32) {
+	return b.sample.values.DiffuseColor, 1.0
+}
+
+func (b *Brdf) ImportanceSample(subsample uint32, sampler sampler.Sampler) (math.Vector3, math.Vector3, float32, float32) {
+	wi := b.sample.values.N.Reflect(b.sample.values.Wo.Scale(-1.0)).Normalized()
+
+	h := b.sample.values.Wo.Add(wi).Normalized()
+	WoDotH := b.sample.values.Wo.Dot(h)
+
+	f0 := math.MakeVector3(0.03, 0.03, 0.03)
+	fresnel := ggx.SpecularF(WoDotH, f0)
+
+	return b.sample.values.DiffuseColor.Mul(fresnel), wi, 1.0, 1.0
+}
